7f41740dbe2ce10e6d37d9b926a1e5bbeacad414
Ilya Averyanov <av@rubybox.ru>
2022-09-28 22:08:34 +0300

    first commit

---
+++ lib/emqx_test/docker_compose.ex
@@ -0,0 +1,115 @@
+defmodule EMQXTest.DockerCompose do
+  alias __MODULE__, as: DC
+
+  use GenServer
+
+  @docker_compose "docker-compose"
+  @line_max 9999
+
+  defstruct port: nil,
+            file: "",
+            env: [],
+            current_line: "",
+            lines: %{}
+
+  def start_link(file, env) do
+    GenServer.start_link(__MODULE__, %{file: file, env: env})
+  end
+
+  def get_logs(pid, tag) do
+    GenServer.call(pid, {:get_logs, tag})
+  end
+
+  def stop(pid) do
+    GenServer.call(pid, :stop)
+  end
+
+  def init(%{file: file, env: env}) do
+    Process.flag(:trap_exit, true)
+
+    port =
+      Port.open(
+        {:spawn_executable, docker_compose()},
+        [
+          :binary,
+          :stderr_to_stdout,
+          args: ["-f", file, "up", "--build", "--no-color"],
+          env: charlist_env(env),
+          line: @line_max
+        ]
+      )
+
+    {:ok, %DC{port: port, file: file, env: env}}
+  end
+
+  def handle_call({:get_logs, tag}, _from, %DC{lines: lines} = st) do
+    log_lines = Map.get(lines, tag, [])
+    {:reply, log_lines, st}
+  end
+
+  def handle_call(:stop, _from, st) do
+    {:stop, :normal, :ok, st}
+  end
+
+  def handle_info({port, {:data, {flag, line}}}, %DC{port: port} = st) do
+    new_st = append_line(flag, line, st)
+    {:noreply, new_st}
+  end
+
+  def handle_info({port, :closed}, %DC{port: port} = st) do
+    {:stop, :closed, st}
+  end
+
+  def handle_info({:EXIT, port, reason}, %DC{port: port} = st) do
+    {:stop, reason, st}
+  end
+
+  def terminate(%DC{port: port, file: file, env: env}) do
+    true = Port.close(port)
+    System.cmd(
+      docker_compose(),
+      ["-f", file, "down", "--remove-orphans"],
+      [{:env, env}, :stderr_to_stdout]
+    )
+  end
+
+  defp docker_compose() do
+    System.find_executable(@docker_compose)
+  end
+
+  defp append_line(:eol, line, %DC{current_line: current_line, lines: lines} = st) do
+    full_line = current_line <> line
+    {tag, log_line} = parse_line(full_line)
+
+    %DC{
+      st
+      | current_line: "",
+        lines: Map.update(lines, tag, [log_line], fn log_lines -> [log_line | log_lines] end)
+    }
+  end
+
+  defp append_line(:noeol, line, %DC{current_line: current_line} = st) do
+    new_current_line = current_line <> line
+    %DC{st | current_line: new_current_line}
+  end
+
+  @dc_log_sep "|"
+  @default_tag ""
+
+  defp parse_line(line) do
+    case String.split(line, @dc_log_sep, parts: 2) do
+      [tag, line] ->
+        {String.trim(tag), String.trim_leading(line)}
+
+      [line] ->
+        {@default_tag, line}
+    end
+  end
+
+  defp charlist_env(env) do
+    Enum.map(
+      env,
+      fn {name, val} -> {to_charlist(name), to_charlist(val)} end
+    )
+  end
+end
---
+++ test/jwt_authz/assets/jwt_server/mix.exs
@@ -0,0 +1,30 @@
+defmodule JwtServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :jwt_server,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {JwtServer.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:jason, "~> 1.3"},
+      {:jose, "~> 1.11"},
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+end
